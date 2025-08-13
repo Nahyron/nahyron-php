1e67851f60f68c14f44f077defe2812f743c6609
nahyron eduardo <nahyron.jesus@aluno.senai.br>
2025-08-13 15:39:26 -0300

    agora criei um input (file) para mandar algum arquivo, e vou puxar isso pro php

--- cadastroFuncionario.php
+++ cadastroFuncionario.php
@@ -10,8 +10,14 @@
 <!-- Os comentários dizem oq os comandos que estão em baixo deles fazem -->
 
 
+
+<h1>Cadastro de funcionários</h1>
+
+
 <!-- Aqui criei um form ja que vou fazer um formulário de perguntas, junto com um fieldset para deixar como um formulário -->
 <form method="POST">
+
+<!-- aqui criei uma área para o formulário, e as informações -->
 
 <fieldset>
 
@@ -74,21 +80,61 @@
 <!-- aqui serve para criar um campo para a pessoa colocar a informação que foi solicitado na linha a cima -->
 
 <input type="number" name="horas" placeholder="Quantas??" required>
+<br><br>
+
+<!-- aqui criei uma área para o envio do currículo -->
+
+<fieldset>
+    
+    <!-- o nome da área -->
+
+    <legend>Currículo</legend>
+
+    <!-- o que será solicitado -->
+    
+    <label name="arquivo">Mande seu currículo</label>
+    <br><br>
+
+    <!-- Comando para a pessoa poder enviar um arquivo -->
+
+    <input type="file" name="arquivo">
 
 
+    <!-- aqui encerra a area do currículo -->
 
 </fieldset>
 
+    <!-- aqui se encerra a area do cadastro -->
 
+</fieldset>
+
+<!-- Criei um botão para enviar -->
 
 <input type="submit" name="enviar">
+
+<!-- e aqui um botão pra limpar os campos de digitação -->
+
 <input type="reset" name="limpar">
+
+
+<!-- aqui se encerra o formulário (front) -->
+
 
 </form>
 
+
+
+<!-- aqui começa o php -->
+
 <?php
 
+
+// aqui serve para puxar o html para o php, verificando se quando a pessoa clicar no botão, o php será executado
+
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+    // as variaves do php puxando os inputs do html para guardar no php
 
 $nome = $_POST["nome"];
 $idade = $_POST["idade"];
@@ -96,9 +142,14 @@ $cep = $_POST ["cep"];
 $genero = $_POST["genero"];
 $dinheiro = $_POST["dinheiro"];
 $horas = $_POST["horas"];
+$arquivo = $_POST["arquivo"];
 
 
- $linha = "$nome | $idade | $cep | $genero | $dinheiro | $horas \n";
+// aqui criei uma variavel, com as variaveis que puxei do html para o php, que é os campos que serão mostrados no arquivo (.txt)
+
+ $linha = "$nome | $idade | $cep | $genero | $dinheiro | $horas | $arquivo \n";
+
+// aqui eu puxo o arquivo que criei e conecto com o php,assim puxando a variavel ($linha) e as variaveis que estão dentro delas para o arquivo (.txt) 
 
   file_put_contents("arquivo/formulario.txt", $linha, FILE_APPEND);
 
@@ -108,7 +159,7 @@ $horas = $_POST["horas"];
 
 
 
-
+// aqui encerra o php
 
 ?>
 
