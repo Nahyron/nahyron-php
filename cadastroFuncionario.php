494fc9f78d03eeecf410d9c82a5f700f52b6de61
nahyron eduardo <nahyron.jesus@aluno.senai.br>
2025-08-14 16:05:11 -0300

    to fazendo algo que eu nem sabiaque era possivel, obrigado raynner toschi fjsddhsjakldha

--- cadastroFuncionario.php
+++ cadastroFuncionario.php
@@ -173,8 +173,59 @@ if($arquivo["error"] === UPLOAD_ERR_OK) {
 }
 
 
-$linha = "$nome | $idade | $cep | $genero | $dinheiro | $horas | $archive \n";
+$linha = "$nome | $idade | $cep | $genero | $dinheiro | $horas |$archive \n";
 file_put_contents("arquivo/formulario.txt", $linha, FILE_APPEND);
+
+        $arquivo = 'arquivo/formulario.txt';
+
+    if (file_exists($arquivo)) {
+        $linhas = file($arquivo);
+        $dados_linhas = [];
+        $max_campos = 0;
+      
+
+    
+        foreach ($linhas as $linha) {
+            $dados = explode('|', trim($linha));
+            $dados_linhas[] = $dados;
+            if (count($dados) > $max_campos) {
+                $max_campos = count($dados);
+            }
+        }
+
+
+      
+        echo "<table border='1' cellpadding='8' cellspacing='0' >";
+
+      
+        echo "<tr>";
+        for ($i = 1; $i <= $max_campos; $i++) {
+            echo "<th>Campo $i</th>";
+        }
+        echo "</tr>";
+        
+  
+        foreach ($dados_linhas as $linha_dados){
+            echo "<tr>";
+            for($i = 0; $i < $max_campos; $i++){
+                $valor = isset($linha_dados[$i]) ? htmlspecialchars($linha_dados[$i]) : '';
+                if($i == 6){
+                    echo "<td><img src='arquivo/$valor' alt='$valor></td>'";
+            }else{
+            echo "<td>$valor</td>";
+        }
+            
+            }
+            echo "</td>";
+        }
+
+        echo "</table>";
+
+
+    } else {
+        echo "Arquivo não encontrado";
+    }
+
 }
 
 
