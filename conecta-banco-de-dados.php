8e54bdc0ff0ad427f2966f62d1829ce3d842c826
nahyron eduardo <nahyron.jesus@aluno.senai.br>
2025-09-03 15:49:30 -0300

    apenas um upd, fiz 3 registros no Mysql

--- conecta-banco-de-dados.php
+++ conecta-banco-de-dados.php
@@ -13,14 +13,25 @@ if (!$conn){
 
 echo "Conectado com succes";
 
-$sql = "CREATE TABLE teste01 (
-idpessoanovaTeste int(11),
-pessoanova_nome varchar(100),
-pessoanova_cpf varchar(14),
-pessoanova_rg varchar(20),
-pessoanova_endereco varchar(150),
-pessoanova_bairro varchar(100),
-pessoanova_cep varchar(9));    ";
+$sql = "INSERT INTO teste01 (
+
+idpessoanovaTeste, 
+pessoanova_nome, 
+pessoanova_cpf, 
+pessoanova_rg, 
+pessoanova_endereco, 
+pessoanova_bairro, 
+pessoanova_cep
+) VALUES (
+    3,
+   'parafal',
+   '92846295874',
+   '47384374323',
+   'rua das paineiras',
+   'bairro do escuro',
+   '15503-022'
+
+);   ";
 
 if(mysqli_query($conn, $sql)){
     echo "<br>Comando executado com sucesso";
